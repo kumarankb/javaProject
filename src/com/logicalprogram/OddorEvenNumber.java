9f948e4fe99b56431614a4cbbba0c20b56dff474
kumarankb <kumaranbalakrishnan028@gmail.com>
2023-06-22 14:33:25 +0530

    first commit

---
+++ src/com/logicalprogram/OddorEvenNumber.java
@@ -0,0 +1,15 @@
+package com.logicalprogram;
+
+public class OddorEvenNumber {
+	public static void main(String[] args) {
+		
+		int n=25;
+		if(n%2==1){
+			System.out.println("given number is odd number");
+		}
+		else {
+			System.out.println("given number is even  number");
+		}
+	}
+
+}
